b8553b7b54bde1148a5d4a9b795d110db52a0159
FahmiEfendy <fahmi.efendy@binus.ac.id>
2022-11-05 13:56:31 +0700

    feat update seeder

--- src/seeder/productList.js
+++ src/seeder/productList.js
@@ -1,184 +1,422 @@
 export const productList = [
+  // Beer
   {
     productPath: "/beer",
     productType: "Beer",
     data: [
       {
-        no: 1,
+        no: `beer-${1}`,
         productName: "Corona Beer Mexico 355ml",
         productPrice: "Rp60.000",
         productImage: require("../assets/ProductImage/Beer/Corona Beer Mexico 355ml.jpg"),
       },
+      {
+        no: `beer-${2}`,
+        productName: "Sapporo Premium Beer Pint Japan 5% alc 330ml",
+        productPrice: "Rp60.000",
+        productImage: require("../assets/ProductImage/Beer/Sapporo Premium Beer Pint Japan 5 alc 330ml.png"),
+      },
     ],
   },
+
+  // Gin
   {
     productPath: "/gin",
     productType: "Gin",
     data: [
       {
-        no: 2,
+        no: `gin-${1}`,
         productName: "Bulldog Gin United Kingdom 750ml",
         productPrice: "Rp870.000",
         productImage: require("../assets/ProductImage/Gin/Bulldog Gin United Kingdom 750ml.webp"),
       },
     ],
   },
+
+  // Other
   {
     productPath: "/other",
     productType: "Other",
     data: [
       {
-        no: 3,
+        no: `other-${1}`,
         productName: "Benedicte Dom",
         productPrice: "Rp1.095.000",
         productImage: require("../assets/ProductImage/Other/Benedicte Dom.png"),
       },
+      {
+        no: `other-${2}`,
+        productName: "Choya Umeshu Single Year 325ml",
+        productPrice: "Rp370.000",
+        productImage: require("../assets/ProductImage/Other/Choya Umeshu Single Year 325ml.jpg"),
+      },
+      {
+        no: `other-${3}`,
+        productName: "Martell VSOP Red Barrel",
+        productPrice: "Rp1.650.000",
+        productImage: require("../assets/ProductImage/Other/Martell VSOP Red Barrel.jpg"),
+      },
     ],
   },
+
+  // Red Wine
   {
     productPath: "/red-wine",
     productType: "Red Wine",
     data: [
       {
-        no: 4,
+        no: `red-wine-${1}`,
         productName: "Alamos Malbec Argentina",
         productPrice: "Rp395.000",
         productImage: require("../assets/ProductImage/Red Wine/Alamos Malbec Argentina.png"),
       },
       {
-        no: 5,
+        no: `red-wine-${2}`,
         productName: "Antigal Estimulo Cabernet Sauvignon",
         productPrice: "Rp320.000",
         productImage: require("../assets/ProductImage/Red Wine/Antigal Estimulo Cabernet Sauvignon.png"),
       },
       {
-        no: 6,
+        no: `red-wine-${3}`,
         productName:
           "Chateau De La Negly la Falaise Rouge 2018 Red Wine France 15.5% alc",
         productPrice: "Rp785.000",
         productImage: require("../assets/ProductImage/Red Wine/Chateau De La Negly la Falaise Rouge 2018 Red Wine France 15.5 alc.png"),
       },
       {
-        no: 7,
+        no: `red-wine-${4}`,
         productName:
           "Chateau Saint Romans 2018 Bordeaux France Red Wine 14% alc 750ml",
         productPrice: "Rp350.000",
         productImage: require("../assets/ProductImage/Red Wine/Chateau Saint Romans 2018 Bordeaux France Red Wine 14 alc 750ml.png"),
       },
       {
-        no: 8,
+        no: `red-wine-${5}`,
         productName: "Clarendelle Rouge 2015 Bordeaux Red Wine France",
         productPrice: "Rp635.000",
         productImage: require("../assets/ProductImage/Red Wine/Clarendelle Rouge 2015 Bordeaux Red Wine France.jfif"),
       },
       {
-        no: 9,
+        no: `red-wine-${6}`,
         productName: "D'Alamel Reserva Cabernet Merlot 2018 Chile",
         productPrice: "Rp400.000",
         productImage: require("../assets/ProductImage/Red Wine/D'Alamel Reserva Cabernet Merlot 2018 Chille.png"),
       },
       {
-        no: 10,
+        no: `red-wine-${7}`,
         productName: "D'Alamel Reserva Cabernet Sauvignon 2018 Chile",
         productPrice: "Rp400.000",
         productImage: require("../assets/ProductImage/Red Wine/D'Alamel Reserva Cabernet Sauvignon 2018 Chille.png"),
       },
       {
-        no: 11,
+        no: `red-wine-${8}`,
         productName: "Decoy Cabernet Sauvignon 2019 California United States",
         productPrice: "Rp720.000",
         productImage: require("../assets/ProductImage/Red Wine/Decoy Cabernet Sauvignon 2019 California United States.jpg"),
       },
+      {
+        no: `red-wine-${9}`,
+        productName: "Decoy Merlot 2019 California United States",
+        productPrice: "Rp720.000",
+        productImage: require("../assets/ProductImage/Red Wine/Decoy Merlot 2019 California United States.jpg"),
+      },
+      {
+        no: `red-wine-${10}`,
+        productName: "Decoy Pinot Noir 2018 California United States",
+        productPrice: "Rp720.000",
+        productImage: require("../assets/ProductImage/Red Wine/Decoy Pinot Noir 2018 California United States.jpg"),
+      },
+      {
+        no: `red-wine-${11}`,
+        productName: "Envynol Merlot 2017 France",
+        productPrice: "Rp380.000",
+        productImage: require("../assets/ProductImage/Red Wine/Envynol Merlot 2017 France.jfif"),
+      },
+      {
+        no: `red-wine-${12}`,
+        productName: "F Negroamaro Salento Italy Red Wine",
+        productPrice: "Rp960.000",
+        productImage: require("../assets/ProductImage/Red Wine/F Negroamaro Salento Italy Red Wine.jpg"),
+      },
+      {
+        no: `red-wine-${13}`,
+        productName: "Farnese Edizione Cinque Autoctoni Italy Red Wine ",
+        productPrice: "Rp960.000",
+        productImage: require("../assets/ProductImage/Red Wine/Farnese Edizione Cinque Autoctoni Italy Red Wine .jpeg"),
+      },
+      {
+        no: `red-wine-${14}`,
+        productName: "Freakshow Cabernet Sauvignon 2017 United States America",
+        productPrice: "Rp850.000",
+        productImage: require("../assets/ProductImage/Red Wine/Freakshow Cabernet Sauvignon 2017 United States America.webp"),
+      },
+      {
+        no: `red-wine-${15}`,
+        productName: "Frontera Merlot Red Wine 2019 Chile",
+        productPrice: "Rp395.000",
+        productImage: require("../assets/ProductImage/Red Wine/Frontera Merlot Red Wine 2019 Chille.png"),
+      },
+      {
+        no: `red-wine-${16}`,
+        productName: "Lambrusco Academia Italy Sweet & Sparkling ",
+        productPrice: "Rp330.000",
+        productImage: require("../assets/ProductImage/Red Wine/Lambrusco Academia Italy Sweet & Sparkling .webp"),
+      },
+      {
+        no: `red-wine-${17}`,
+        productName: "Lambrusco Emilia Terre Frizzante Italy Sweet & Sparkling",
+        productPrice: "Rp365.000",
+        productImage: require("../assets/ProductImage/Red Wine/Lambrusco Emilia Terre Frizzante Italy Sweet & Sparkling.jpg"),
+      },
+      {
+        no: `red-wine-${18}`,
+        productName: "Lambrusco Roccaforte Italy Sweet & Sparkling",
+        productPrice: "Rp270.000",
+        productImage: require("../assets/ProductImage/Red Wine/Lambrusco Roccaforte Italy Sweet & Sparkling.webp"),
+      },
+      {
+        no: `red-wine-${19}`,
+        productName: "Lambrusco Sababay Bali Sweet & Sparkling",
+        productPrice: "Rp260.000",
+        productImage: require("../assets/ProductImage/Red Wine/Lambrusco Sababay Bali Sweet & Sparkling.webp"),
+      },
+      {
+        no: `red-wine-${20}`,
+        productName: "Lindeman 50 Shiraz Australia",
+        productPrice: "Rp305.000",
+        productImage: require("../assets/ProductImage/Red Wine/Lindeman 50 Shiraz Australia.jpg"),
+      },
+      {
+        no: `red-wine-${21}`,
+        productName:
+          "Lindeman's Gentleman's Collection Cabernet Sauvignon No. 3",
+        productPrice: "Rp600.000",
+        productImage: require("../assets/ProductImage/Red Wine/Lindeman's Gentleman's Collection Cabernet Sauvignon No. 3.webp"),
+      },
+      {
+        no: `red-wine-${22}`,
+        productName: "Mascetti Sababay Port Style Bali",
+        productPrice: "Rp425.000",
+        productImage: require("../assets/ProductImage/Red Wine/Mascetti Sababay Port Style Bali.webp"),
+      },
+      {
+        no: `red-wine-${23}`,
+        productName: "Mi Terruno Reserve Cabernet Sauvignon",
+        productPrice: "Rp430.000",
+        productImage: require("../assets/ProductImage/Red Wine/Mi Terruno Reserve Cabernet Sauvignon.png"),
+      },
+      {
+        no: `red-wine-${24}`,
+        productName: "Montes Alpha Cabernet Sauvignon Chile",
+        productPrice: "Rp675.000",
+        productImage: require("../assets/ProductImage/Red Wine/Montes Alpha Cabernet Sauvignon Chille.webp"),
+      },
+      {
+        no: `red-wine-${25}`,
+        productName: "Montes Alpha Merlot Chile",
+        productPrice: "Rp675.000",
+        productImage: require("../assets/ProductImage/Red Wine/Montes Alpha Merlot Chile.png"),
+      },
+      {
+        no: `red-wine-${26}`,
+        productName: "Moulin De Gassac Red France",
+        productPrice: "Rp330.000",
+        productImage: require("../assets/ProductImage/Red Wine/Moulin De Gassac Red France.png"),
+      },
+      {
+        no: `red-wine-${27}`,
+        productName: "Sessantanni Primitivo Di Manduria Italy Red Wine",
+        productPrice: "Rp960.000",
+        productImage: require("../assets/ProductImage/Red Wine/Sessantanni Primitivo Di Manduria Italy Red Wine.webp"),
+      },
+      {
+        no: `red-wine-${28}`,
+        productName: "Sterling Vintner's Cabernet Sauvignon USA",
+        productPrice: "Rp500.000",
+        productImage: require("../assets/ProductImage/Red Wine/Sterling Vintner's Cabernet Sauvignon USA.webp"),
+      },
+      {
+        no: `red-wine-${29}`,
+        productName: "Sterling Vintner's Merlot USA",
+        productPrice: "Rp500.000",
+        productImage: require("../assets/ProductImage/Red Wine/Sterling Vintner's Merlot USA.png"),
+      },
+      {
+        no: `red-wine-${30}`,
+        productName: "Yomeishu Herbal Tonic Wine Japan 1ltr",
+        productPrice: "Rp1.000.000",
+        productImage: require("../assets/ProductImage/Red Wine/Yomeishu Herbal Tonic Wine Japan.jpg"),
+      },
+      {
+        no: `red-wine-${31}`,
+        productName: "Yomeishu Herbal Tonic Wine Japan 700ml",
+        productPrice: "Rp850.000",
+        productImage: require("../assets/ProductImage/Red Wine/Yomeishu Herbal Tonic Wine Japan.jpg"),
+      },
     ],
   },
+
+  // Rose Wine
   {
     productPath: "/rose-wine",
     productType: "Rose Wine",
     data: [
       {
-        no: 12,
+        no: `rose-wine-${1}`,
+        productName: "Mateus Rose Wine Portugal",
+        productPrice: "Rp380.000",
+        productImage: require("../assets/ProductImage/Rose Wine/Mateus Rose Wine Portugal.png"),
+      },
+      {
+        no: `rose-wine-${2}`,
         productName: "Miraval Provence Rose France (Rose Wine)",
         productPrice: "Rp800.000",
         productImage: require("../assets/ProductImage/Rose Wine/Miraval Provence Rose France (Rose Wine).jfif"),
       },
     ],
   },
+
+  // Soju
   {
     productPath: "/soju",
     productType: "Soju",
     data: [
       {
-        no: 13,
-        productName: "Chum Churum Apple Mango",
-        productPrice: "Rp395.000",
-        productImage: require("../assets/ProductImage/Soju/Chum Churum Apple Manggo.png"),
+        no: `soju-${1}`,
+        productName: "20 x Jinro Chamisul Same Flavour",
+        productPrice: "Rp1.960.000",
+        productImage: require("../assets/ProductImage/Soju/20 x Jinro Chamisul Same Flavour.png"),
       },
       {
-        no: 14,
+        no: `soju-${2}`,
         productName: "Chum Churum Apple",
         productPrice: "Rp100.000",
         productImage: require("../assets/ProductImage/Soju/Chum Churum Apple.png"),
       },
       {
-        no: 15,
+        no: `soju-${3}`,
+        productName: "Chum Churum Apple Mango",
+        productPrice: "Rp100.000",
+        productImage: require("../assets/ProductImage/Soju/Chum Churum Apple Manggo.png"),
+      },
+      {
+        no: `soju-${4}`,
         productName: "Chum Churum Blueberry",
         productPrice: "Rp100.000",
         productImage: require("../assets/ProductImage/Soju/Chum Churum Blueberry.png"),
       },
       {
-        no: 16,
+        no: `soju-${5}`,
         productName: "Chum Churum Original",
         productPrice: "Rp100.000",
         productImage: require("../assets/ProductImage/Soju/Chum Churum Original.jfif"),
       },
       {
-        no: 17,
+        no: `soju-${6}`,
         productName: "Chum Churum Peach",
         productPrice: "Rp100.000",
         productImage: require("../assets/ProductImage/Soju/Chum Churum Peach.png"),
       },
       {
-        no: 18,
+        no: `soju-${7}`,
         productName: "Chum Churum Yoghurt",
         productPrice: "Rp100.000",
         productImage: require("../assets/ProductImage/Soju/Chum Churum Yoghurt.jpg"),
       },
       {
-        no: 19,
+        no: `soju-${8}`,
         productName: "Jinro Chamisul Fresh - Original",
         productPrice: "Rp105.000",
         productImage: require("../assets/ProductImage/Soju/Jinro Chamisul Fresh - Original.jpg"),
       },
       {
-        no: 20,
+        no: `soju-${9}`,
         productName: "Jinro Chamisul Grape Fruit",
-        productType: "Soju",
         productPrice: "Rp105.000",
         productImage: require("../assets/ProductImage/Soju/Jinro Chamisul Grape Fruit.webp"),
       },
+      {
+        no: `soju-${10}`,
+        productName: "Jinro Chamisul Green Grape",
+        productPrice: "Rp105.000",
+        productImage: require("../assets/ProductImage/Soju/Jinro Chamisul Green Grape.webp"),
+      },
+      {
+        no: `soju-${11}`,
+        productName: "Jinro Chamisul Plum",
+        productPrice: "Rp105.000",
+        productImage: require("../assets/ProductImage/Soju/Jinro Chamisul Plum.webp"),
+      },
+      {
+        no: `soju-${12}`,
+        productName: "Jinro Chamisul Strawberry",
+        productPrice: "Rp105.000",
+        productImage: require("../assets/ProductImage/Soju/Jinro Chamisul Strawberry.jpg"),
+      },
     ],
   },
+
+  // Whisky
   {
     productPath: "/whisky",
     productType: "Whisky",
     data: [
       {
-        no: 21,
+        no: `whisky-${1}`,
         productName: "Black Douglas Single Grain Scotch Whisky 40%",
         productPrice: "Rp380.000",
         productImage: require("../assets/ProductImage/Whisky/Black Douglas Single Grain Scotch Whisky 40.png"),
       },
+      {
+        no: `whisky-${2}`,
+        productName:
+          "Macallan 12 Years Double Cask Single Malt Scotchland Whisky",
+        productPrice: "Rp3.000.000",
+        productImage: require("../assets/ProductImage/Whisky/Macallan 12 Sherry Oak Single Malt Schotchland Whisky.jpg"),
+      },
+      {
+        no: `whisky-${3}`,
+        productName: "Macallan 12 Sherry Oak Single Malt Schotchland Whisky",
+        productPrice: "Rp3.500.000",
+        productImage: require("../assets/ProductImage/Whisky/Macallan 12 Years Double Cask Single Malt Scotchland Whisky.webp"),
+      },
     ],
   },
+
+  // White Wine
   {
     productPath: "/white-wine",
     productType: "White Wine",
     data: [
       {
-        no: 22,
+        no: `whisky-${1}`,
         productName: "30 Mile Chardonnay Australia White Wine",
         productPrice: "Rp320.000",
         productImage: require("../assets/ProductImage/White Wine/30 Mile Chardonnay Australia White Wine.webp"),
+      },
+      {
+        no: `whisky-${2}`,
+        productName: "30 Mile Sauvignon Blanc",
+        productPrice: "Rp320.000",
+        productImage: require("../assets/ProductImage/White Wine/30 Mile Sauvignon Blanc.webp"),
+      },
+      {
+        no: `whisky-${3}`,
+        productName: "Frontera Chardonnay White Wine 2020 Chile",
+        productPrice: "Rp395.000",
+        productImage: require("../assets/ProductImage/White Wine/Frontera Chardonnay White Wine 2020 Chille.jfif"),
+      },
+      {
+        no: `whisky-${4}`,
+        productName: "Frontera Late Harvest Sweet White Wine Chile",
+        productPrice: "Rp395.000",
+        productImage: require("../assets/ProductImage/White Wine/Frontera Late Harvest Sweet White Wine Chille.jfif"),
+      },
+      {
+        no: `whisky-${5}`,
+        productName: "Montes Alpha Chardonnay",
+        productPrice: "Rp675.000",
+        productImage: require("../assets/ProductImage/White Wine/Montes Alpha Chardonnay.webp"),
       },
     ],
   },
